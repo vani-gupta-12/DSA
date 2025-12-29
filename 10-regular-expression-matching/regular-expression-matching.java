59443c7cbd0606b178e85dd28c49314c60ac6156
vani-gupta-12 <guptavani2005@gmail.com>
2025-12-29 23:39:16 +0530

    Time: 2 ms (55.14%) | Memory: 43.9 MB (18.76%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        // handle patterns like a*, a*b*, a*b*c*
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                // Case 1: characters match OR '.' matches anything
+                if (p.charAt(j - 1) == s.charAt(i - 1) ||
+                    p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                // Case 2: '*' pattern
+                else if (p.charAt(j - 1) == '*') {
+
+                    // zero occurrences of previous character
+                    dp[i][j] = dp[i][j - 2];
+
+                    // one or more occurrences
+                    if (p.charAt(j - 2) == s.charAt(i - 1) ||
+                        p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
