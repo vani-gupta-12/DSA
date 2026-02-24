73570630e59f7f5e4b14b21894555b860b3e6fcc
vani-gupta-12 <guptavani2005@gmail.com>
2026-02-24 19:42:48 +0530

    Time: 354 ms (70.48%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select email from person group by email having count(email) > 1;
+/*select email from person group by email having count(email) > 1;*/
+select email as Email from  (select email from person group by email having count(email)>1) as duplicate;
