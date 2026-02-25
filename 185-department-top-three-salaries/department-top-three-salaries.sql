29cf444c46805621e2a69c7ee463c0ac2a3d9b46
vani-gupta-12 <guptavani2005@gmail.com>
2026-02-25 12:51:41 +0530

    Time: 1294 ms (18.15%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select d.name as Department, e.name as Employee , e.salary as Salary
+from Employee e 
+join Department d on e.departmentId = d.id 
+where (select count(distinct(e2.salary)) from  Employee e2 
+where e2.departmentId = e.departmentId 
+and e2.salary> e.salary)<3;
