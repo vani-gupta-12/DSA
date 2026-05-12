824ed7c8f57b85be0a098cee7487c8cd01755571
vani-gupta-12 <guptavani2005@gmail.com>
2026-05-12 14:25:56 +0530

    Time: 424 ms (5.02%) | Memory: 237.3 MB (6.80%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -3,8 +3,8 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         dp = new int[n][n+1];
-        for (int i=0;i<n;i++){
-            for(int j=0;j<=n;j++){
+        for (int i=n-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
                 dp[i][j] = -1;
             }
         }
