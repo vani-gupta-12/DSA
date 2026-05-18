ad4bd5526e9c7e4f901adb07afc1070dd3d1f89a
vani-gupta-12 <guptavani2005@gmail.com>
2026-05-18 12:23:04 +0530

    Time: 2 ms (10.81%) | Memory: 46.7 MB (60.06%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,40 +1,37 @@
 class Solution {
     class Pair{
-        int row ;
+        int row;
         int col;
         Pair(int row, int col){
             this.row = row;
             this.col = col;
         }
     }
-    public boolean isValid(int row, int col, int tRows, int tCols){
-        if (row >=0 && row<tRows && col >= 0 && col<tCols) return true;
-        return false;
-    }
-    int rowD[] = {-1,0,0,1};
-    int colD[] = {0,1,-1,0};
+    int[] rd = {0,0,-1,1};
+    int[] cd = {-1,1,0,0};
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int or = image[sr][sc];
-        if (or==color) return image;
-        Queue<Pair> q = new LinkedList<>(); 
         int n = image.length;
         int m = image[0].length;
-        boolean isVis[][] = new boolean[n][m];
+        boolean vis[][] = new boolean[n][m];
+        int original = image[sr][sc];
+        if(original==color) return image;
+        Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(sr,sc));
-        isVis[sr][sc] = true;
+        vis[sr][sc] = true;
         while (!q.isEmpty()){
             Pair curr = q.poll();
-            int currRow = curr.row;
-            int currCol = curr.col;
-            image[currRow][currCol] = color;
-            for (int i=0; i<4;i++){
-                int newRow = currRow + rowD[i];
-                int newCol = currCol +colD[i];
-                if (isValid(newRow, newCol, n,m) && image[newRow][newCol]==or && isVis[newRow][newCol]==false){
-                    isVis[newRow][newCol] = true;
-                    q.add(new Pair(newRow,newCol));
+            int cr = curr.row;
+            int cc = curr.col;
+            image[cr][cc] = color;
+            for(int i=0; i<4;i++){
+                int nr = cr + rd[i];
+                int nc = cc + cd[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc] == original && !vis[nr][nc]){
+                    q.add(new Pair(nr,nc));
+                    vis[nr][nc] = true;
                 }
             }
+            
         }
         return image;
     }
