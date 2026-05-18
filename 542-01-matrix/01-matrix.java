abbe4cf6a203b99ab8f8ed5c7a08d0dda80ee5e7
vani-gupta-12 <guptavani2005@gmail.com>
2026-05-18 12:34:45 +0530

    Time: 17 ms (19.04%) | Memory: 48.3 MB (93.62%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,41 +1,40 @@
 class Solution {
     class Pair{
-        int row ;
+        int row;
         int col;
         Pair(int row, int col){
             this.row = row;
             this.col = col;
         }
     }
-    int[] rd  = {-1,1,0,0};
-    int[] cd = {0,0,1,-1};
+    int[] rd = {0,0,-1,1};
+    int[] cd = {1,-1,0,0};
     public int[][] updateMatrix(int[][] mat) {
-        int n =mat.length;
+        int n = mat.length;
         int m = mat[0].length;
-        Queue<Pair> q = new LinkedList<>();
-        int[][] ans = new int[n][m];
         boolean[][] vis = new boolean[n][m];
-        for (int i=0;i<n;i++){
-            for (int j=0; j<m;j++){
-                if (mat[i][j]==0){
-                    vis[i][j]= true;
+        Queue<Pair> q = new LinkedList<>();
+        int ans[][] = new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0; j<m;j++){
+                if(mat[i][j] == 0){
                     q.add(new Pair(i,j));
-                    ans[i][j]=0;
+                    vis[i][j] = true;
+                    ans[i][j] = 0;
                 }
             }
-        
         }
-        while (!q.isEmpty()){
+        while(!q.isEmpty()){
             Pair curr = q.poll();
             int cr = curr.row;
             int cc = curr.col;
-            for (int i=0; i<4;i++){
+            for(int i=0; i<4;i++){
                 int nr = cr + rd[i];
                 int nc = cc + cd[i];
-                if (nr>=0 && nr<n && nc>=0 && nc<m && mat[nr][nc]==1 && !vis[nr][nc]){
-                    vis[nr][nc] = true;
-                    ans[nr][nc] = ans[cr][cc]+ 1;
+                if(nr<n && nr>=0 && nc<m && nc>=0 && mat[nr][nc]==1 && !vis[nr][nc]){
                     q.add(new Pair(nr,nc));
+                    vis[nr][nc]=true;
+                    ans[nr][nc] = ans[cr][cc]+1;
                 }
             }
         }
