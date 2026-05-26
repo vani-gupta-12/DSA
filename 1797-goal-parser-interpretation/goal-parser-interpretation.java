5bff31ef2c019349cab33fd85989d5435e14776a
vani-gupta-12 <guptavani2005@gmail.com>
2026-05-26 14:56:42 +0530

    Time: 1 ms (70.40%) | Memory: 43 MB (49.97%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -1,7 +1,19 @@
 class Solution {
     public String interpret(String command) {
-        command = command.replace("()","o");
-        command = command.replace("(al)", "al");
-        return command;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<command.length(); i++){
+            char c = command.charAt(i);
+            if(c == 'G') sb.append('G');
+            else if(c== '('){
+                if(command.charAt(i+1) == ')'){
+                    sb.append('o');
+                    i++;
+                }else{
+                    sb.append("al");
+                    i+=3;
+                }
+            }
+        }
+        return sb.toString();
     }
 }
