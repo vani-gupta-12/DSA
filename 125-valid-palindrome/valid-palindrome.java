d355e71a9505a56f291a4feb94fc398fecb3f1e6
vani-gupta-12 <guptavani2005@gmail.com>
2026-07-06 16:35:34 +0530

    Time: 16 ms (14.38%) | Memory: 47.6 MB (10.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,11 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s= s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-        return pal(s,0);
-    }
-    public boolean pal(String s, int i){
-        if (i >= s.length()/2) return true;
-        if (s.charAt(i) != s.charAt(s.length()-i-1)) return false;
-        return pal(s, i+1);
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+        String rev = new StringBuilder(s).reverse().toString();
+        return (s.equals(rev));
     }
 }
