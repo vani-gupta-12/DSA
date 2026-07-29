b336e2c250e400b680f07296bf79877f20fc6770
vani-gupta-12 <guptavani2005@gmail.com>
2026-07-29 19:37:14 +0530

    Time: 46 ms (90.95%) | Memory: 281.5 MB (60.52%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,60 +1,28 @@
 class Solution {
-    public boolean dfs(int n,int source, ArrayList<Integer> ans, boolean[] isVis, int destination,ArrayList<ArrayList<Integer>> adj, int[][] edges){
-        if (source == destination) return true;
-        isVis[source]=true;
-        for (int i=0;i<adj.get(source).size();i++){
-            int neigh = adj.get(source).get(i);
-            if (neigh == destination) return true;
-            if (isVis[neigh]==false){
-                if (dfs(n, neigh, ans, isVis, destination, adj, edges))
-                return true;
-            }
-        }
-        return false;
-    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-    //BFS
-        /*if (source == destination) return true;
+        if(source==destination) return true;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
-       
-        for (int i=0; i<n;i++){
-            adj.add(new ArrayList<>());
-        }
-        for (int i= 0; i<edges.length;i++){
-            int from = edges[i][0];
-            int to = edges[i][1];
-            adj.get(from).add(to);
-            adj.get(to).add(from);
-        }
-        boolean isVis[] = new boolean[adj.size()];
-        q.add(source);
-        isVis[source] = true;
-        while (!q.isEmpty()){
-            int curr = q.poll();
-            for (int i=0; i<adj.get(curr).size();i++){
-                int neighbour = adj.get(curr).get(i);
-                if (neighbour == destination) return true;
-                if (isVis[neighbour]==false){
-                    isVis[neighbour]=true;
-                    q.add(neighbour);
-                }
-            }
-        }
-        return false;*/
-
-//DFS
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i=0;i<n;i++) adj.add(new ArrayList<>());
-        for (int i=0;i<edges.length;i++){
+        for(int i=0; i<n;i++) adj.add(new ArrayList<>());
+        for(int i=0; i<edges.length;i++){
             int u = edges[i][0];
             int v = edges[i][1];
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-        ArrayList<Integer> ans = new ArrayList<>();
-        int a = adj.size();
-        boolean[] isVis = new boolean[a];
-        return dfs(n,source,ans, isVis, destination, adj, edges);
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[adj.size()];
+        vis[source] = true;
+        q.add(source);
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            for(int neigh : adj.get(curr)){
+                if(!vis[neigh]){
+                    if(neigh==destination) return true;
+                    q.add(neigh);
+                    vis[neigh] = true;
+                }
+            }
+        }   
+        return false;
     }
 }
