deeddffb0735fdda980ab50e1b45cd3fe5bf6e0d
vani-gupta-12 <guptavani2005@gmail.com>
2026-07-30 23:11:17 +0530

    Time: 1 ms (86.72%) | Memory: 47.3 MB (59.48%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,21 +1,20 @@
 class Solution {
-    int n;
     public int findCircleNum(int[][] isConnected) {
-        n = isConnected.length;
+        int count = 0;
+        int n = isConnected.length;
         boolean[] vis = new boolean[n];
-        int count=0;
-        for(int i=0;i<n;i++){
+        for(int i=0; i<n;i++){
             if(!vis[i]){
-                dfs(i,vis, isConnected);
+                dfs(isConnected, i, vis, n);
                 count++;
-            }            
+            }
         }
         return count;
     }
-    public void dfs(int u, boolean[] vis, int[][] isConnected){
-        vis[u] = true;
+    public void dfs(int[][] isConnected, int u, boolean[] vis, int n){
+        vis[u]=true;
         for(int v=0; v<n;v++){
-            if(!vis[v] && isConnected[u][v]==1) dfs(v,vis,isConnected);
+            if(!vis[v] && isConnected[u][v]==1) dfs(isConnected,v,vis,n);
         }
     }
 }
