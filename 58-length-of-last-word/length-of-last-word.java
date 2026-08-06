e2c3f4642a3e9d0aca1f7c9e923c0b7f58d6a14c
vani-gupta-12 <guptavani2005@gmail.com>
2026-08-06 17:38:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (93.15%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        //if(s.equals(" ")) return 0;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i)==' '){
+                if(count==0) continue; 
+                else break;
+            } 
+            else count++;
+        }
+        return count;
+    }
+}
